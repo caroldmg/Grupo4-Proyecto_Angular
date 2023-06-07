507d72e76a11fa106ca0b38106a0a133b981e1bd
caroldmg <caroldmg98@gmail.com>
2023-06-08 00:27:21 +0200

    creacion modelo provincia y actualizar db con api

--- Grupo4_angular/README.md
+++ Grupo4_angular/README.md
@@ -7,13 +7,26 @@
  * ng generate interface models/recomendation --type=model --prefix=I
  * ng generate interface models/user --type=model --prefix=I
  * ng generate interface models/location --type=model --prefix=I
- * ng generate interface models/weather --type=model --prefix=I
+ 
+-- Estos modelos son para traer la info según la devolución de eltiempo.net
+ * ng generate interface models/provincia --type=model --prefix=I
 
  `Creación del servicio:`
  * ng new service services/user 
+  * ng new service services/weather
 
 ------
  `Para probar las operaciones CRUD voy a hacer una base de datos fake en json ` 
  * npm install -g json-server
  * en mockaroo.com creo 10 json aleatorios con los atributos de IUser
  * para acceder a ellos con httpClient: json-server --watch user-database.json
+
+
+
+ ## API
+ * Veréis que en el weatherService hay una variable apiKey, esa es para por si al final usamos aemet
+ * De momento, voy a ir probando con la de eltiempo.net, que saca los datos desde AEMET pero parece más sencillo acceder a la información desde ahí
+
+ ### API -- ELTIEMPO.NET
+ * los códigos de provincia los tenemos almacenados en db.json
+ * 
--- Grupo4_angular/src/app/services/weather.service.ts
+++ Grupo4_angular/src/app/services/weather.service.ts
@@ -1,11 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IProvincia } from '../models/provincia.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
   
-  apiKey: string = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjYXJvbGRtZzk4QGdtYWlsLmNvbSIsImp0aSI6Ijk0MjU3ODY3LWM3OGYtNGE1Zi04MTBkLTE3ZDExYjhiZDk4MSIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjg2MDg1NjkxLCJ1c2VySWQiOiI5NDI1Nzg2Ny1jNzhmLTRhNWYtODEwZC0xN2QxMWI4YmQ5ODEiLCJyb2xlIjoiIn0.IkoFmqUF2Q3vp35I1y_9Zw913qy_hrA-DybRAbfD14A"
+  apiKey: string = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjYXJvbGRtZzk4QGdtYWlsLmNvbSIsImp0aSI6Ijk0MjU3ODY3LWM3OGYtNGE1Zi04MTBkLTE3ZDExYjhiZDk4MSIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNjg2MDg1NjkxLCJ1c2VySWQiOiI5NDI1Nzg2Ny1jNzhmLTRhNWYtODEwZC0xN2QxMWI4YmQ5ODEiLCJyb2xlIjoiIn0.IkoFmqUF2Q3vp35I1y_9Zw913qy_hrA-DybRAbfD14A" //esta es la apiKey de aemet-openData
 
-  constructor() { }
+  jsonUrl: string = "http://localhost:3000/provincias"
+
+  constructor(private httpClient: HttpClient) { }
+
+  findAllProvincias(): Observable <IProvincia[]>{
+    return this.httpClient.get<IProvincia[]>(this.jsonUrl);
+  }
 }
